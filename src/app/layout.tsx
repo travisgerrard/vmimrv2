e150d9ce0932ca2c89ed71d400d89ca6438b5fdf
Travis <travisgerrard@gmail.com>
2025-04-28 13:38:28 -0700

    chore(favicon): add multi-format favicon and manifest links to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,11 @@ export default function RootLayout({
       <head>
         <title>Personal Medical Reference</title>
         <meta name="description" content="Manage your personal medical information securely." />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         {/* Add other head elements */}
       </head>
       <body
